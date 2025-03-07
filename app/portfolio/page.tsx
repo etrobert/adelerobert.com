c319caa2ce4c5b0fcb170c1cabf3f21166940310
Etienne Robert <etiennerobert33@gmail.com>
2025-03-07 18:37:48 -0300

    move photos arounds and enrich portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,3 +1,34 @@
+import Image from 'next/image';
+import img1 from '../../photos/20240224-DSCF4034.jpeg';
+import img2 from '../../photos/20240220-DSCF3873.jpeg';
+import img3 from '../../photos/20241012-164751-4181-Adele Robert.jpg';
+import img4 from '../../photos/20241013-133244-4292-Adele Robert.jpg';
+
 export default function Portfolio() {
-  return 'Portfolio';
+  return (
+    <div className="grid gap-4 p-6">
+      <h1 className="text-4xl font-bold">Portfolio</h1>
+      <p>This is a short introduction</p>
+      <Image
+        src={img1}
+        // TODO: Update alt text
+        alt="TODO"
+      />
+      <Image
+        src={img2}
+        // TODO: Update alt text
+        alt="TODO"
+      />
+      <Image
+        src={img3}
+        // TODO: Update alt text
+        alt="TODO"
+      />
+      <Image
+        src={img4}
+        // TODO: Update alt text
+        alt="TODO"
+      />
+    </div>
+  );
 }
--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import landingPageImage from './landing-page-image.jpeg';
+import landingPageImage from '../photos/20241013-133244-4292-Adele Robert.jpg';
 
 export default function LandingPage() {
   return (
-    <div className="grid place-items-center h-full text-4xl lg:text-7xl">
+    <div className="grid text-black grid-cols-[2fr_1fr] place-items-center h-full text-4xl lg:text-7xl">
       <Image
         className="absolute z-[-1] h-full w-full object-cover"
         src={landingPageImage}
