0c98c2ce4314da0a3c52fb83b420e2323a4c2f41
Etienne Robert <etiennerobert33@gmail.com>
2025-03-07 12:34:48 -0300

    fix font size on mobile

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -5,7 +5,7 @@ import landingPageImage from './landing-page-image.jpeg';
 
 export default function LandingPage() {
   return (
-    <div className="grid place-items-center h-full text-7xl">
+    <div className="grid place-items-center h-full text-4xl lg:text-7xl">
       <Image
         className="absolute z-[-1] h-full w-full object-cover"
         src={landingPageImage}
