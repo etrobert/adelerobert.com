8ea176d3ed89c3934728ba93e29dc7440c9bdd98
Etienne Robert <etiennerobert33@gmail.com>
2025-03-13 12:41:07 +0100

    udpate portfolio to use Collection

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,4 +1,5 @@
-import Image from 'next/image';
+import Collection from '../../components/Collection';
+
 import img1 from '../../photos/20240224-DSCF4034.jpeg';
 import img2 from '../../photos/20240220-DSCF3873.jpeg';
 import img3 from '../../photos/20241012-164751-4181-Adele Robert.jpg';
@@ -6,33 +7,10 @@ import img4 from '../../photos/20241013-133244-4292-Adele Robert.jpg';
 
 export default function Portfolio() {
   return (
-    <div className="grid gap-4 p-6">
-      <h1 className="text-4xl font-bold">Portfolio</h1>
-      <p>This is a short introduction</p>
-      <Image
-        src={img1}
-        placeholder="blur"
-        // TODO: Update alt text
-        alt="TODO"
-      />
-      <Image
-        src={img2}
-        placeholder="blur"
-        // TODO: Update alt text
-        alt="TODO"
-      />
-      <Image
-        src={img3}
-        placeholder="blur"
-        // TODO: Update alt text
-        alt="TODO"
-      />
-      <Image
-        src={img4}
-        placeholder="blur"
-        // TODO: Update alt text
-        alt="TODO"
-      />
-    </div>
+    <Collection
+      title="Portfolio"
+      description="This is a short introduction."
+      images={[img1, img2, img3, img4]}
+    />
   );
 }
