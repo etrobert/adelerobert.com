20fbe0101c8d03c323c442db923b4671d914420d
Etienne Robert <etiennerobert33@gmail.com>
2025-03-30 14:23:31 +0200

    other try for mobile prevention

--- components/MyImage.tsx
+++ components/MyImage.tsx
@@ -10,6 +10,6 @@ export const MyImage = ({ img }: { img: StaticImport }) => (
     // TODO: Update alt text
     alt="TODO"
     onContextMenu={(e) => e.preventDefault()}
-    onTouchStart={(e) => e.preventDefault()}
+    className="select-none"
   />
 );
