5df09c9ead8e4a5bf0331ce75a23a8777591fc36
Etienne Robert <etiennerobert33@gmail.com>
2026-05-15 16:02:00 +0200

    add image remotePatterns for adele assets

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'files.etiennerobert.com',
+        pathname: '/adele/**',
+      },
+    ],
   },
 };
 
